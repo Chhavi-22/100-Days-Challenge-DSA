9a990347fd8b3c175c33340b92b54a6f2d70148a
Chhavi-22 <sonusagatani@gmail.com>
2026-02-23 12:24:59 +0530

    Refactor C program for inserting an element in array

--- 100 Days Challenge DSA/Day-01-Q1.c
+++ 100 Days Challenge DSA/Day-01-Q1.c
@@ -1,24 +1,4 @@
 //Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
-/*
-Input:
-- First line: integer n
-- Second line: n space-separated integers (the array)
-- Third line: integer pos (1-based position)
-- Fourth line: integer x (element to insert)
-
-Output:
-- Print the updated array (n+1 integers) in a single line, space-separated
-
-Example:
-Input:
-5
-1 2 4 5 6
-3
-3
-
-Output:
-1 2 3 4 5 6 
-*/
 #include <stdio.h>
 int main()
 {
@@ -53,3 +33,4 @@ int main()
 
     return 0;
 }
+
